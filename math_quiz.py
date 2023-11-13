1b154256e0f2a8d56c44b38422ce5e05dec2a858
JeevithPavithran <150521703+JeevithPavithran@users.noreply.github.com>
2023-11-13 20:31:06 +0100

    math_quiz.py

--- math_quiz.py
+++ math_quiz.py
@@ -1,46 +1,46 @@
 import random
 
 
-def function_A(min, max):
+def random_number(min, max):
     """
     Random integer.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def random_operator():
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def calculation(number1, number2, operator):
+    problem = f"{number1} {operator} {number2}"
+    if operator == '+': answer = number1 + number2
+    elif operator == '-': answer = number1 - number2
+    else: answer = number1 * number2
+    return problem, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    score = 0
+    no_of_questions = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(no_of_questions):
+        number1 = random_number(1, 10); number2 = random_number(1, 5); operator = random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = calculation(number1, number2, operator)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{no_of_questions}")
 
 if __name__ == "__main__":
     math_quiz()
