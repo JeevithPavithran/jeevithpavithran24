fd6ec7e847f2b52e917dd1cc1428129542f3e856
JeevithPavithran <150521703+JeevithPavithran@users.noreply.github.com>
2023-11-13 22:58:21 +0100

    Add files via upload

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,7 +1,5 @@
 import unittest
-from math_quiz import random_number, random_operator, calculation
-
-
+from math_quiz import random_integer, random_operator, operation
 class TestMathGame(unittest.TestCase):
 
     def random_integer(self):
@@ -9,7 +7,7 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num =  random_number(min, max)
+            rand_num =  random_integer(min, max)
             self.assertTrue(min <= rand_num <= max)
 
     def random_operator(self):
@@ -17,7 +15,7 @@ class TestMathGame(unittest.TestCase):
          self.assertIn(rand_operator, operators)
         pass
 
-    def calcultion(self):
+    def operation(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
                 (10, 5, '-', '10 - 5', 5),
@@ -26,9 +24,8 @@ class TestMathGame(unittest.TestCase):
             ]
 
             for number1, number2, operator, expected_problem, expected_answer in test_cases:
-                result = calculation(num1, num2, operator)
+                result = operation(num1, num2, operator)
                 self.assertEqual(result, expected_answer)
                 pass
-
 if _name_ == "_main_":
     unittest.main()
