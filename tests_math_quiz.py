110e53be7d19c51f9955bcf3d9f8b36185f2bb20
JeevithPavithran <150521703+JeevithPavithran@users.noreply.github.com>
2023-11-13 21:25:52 +0100

    Update tests_math_quiz.py

--- tests_math_quiz.py
+++ tests_math_quiz.py
@@ -1,30 +1,34 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import random_integer, random_operator, calculation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num =  random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
+    def random_operator(self):
+        rand_operator = random_operator()
+         self.assertIn(rand_operator, operators)
         pass
 
-    def test_function_C(self):
+    def calcultion(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (10, 5, '-', '10 - 5', 5),
+                (3, 6, '*', '3 * 6', 18),
+                (8, 4, '/', '8 / 4', 2),
             ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
+            for number1, number2, operator, expected_problem, expected_answer in test_cases:
+                result = calculation(num1, num2, operator)
+                self.assertEqual(result, expected_answer)
                 pass
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     unittest.main()
